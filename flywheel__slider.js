506af8c1c91da5c89787728722e237fb8bff3bd7
Artem Kopytok <13ar.bass@gmail.com>
2022-04-25 17:28:45 +0300

    updated: value slider code

--- flywheel__slider.js
+++ flywheel__slider.js
@@ -1,12 +1,11 @@
-let targetNode = document.querySelector('.value-slider');
+let targetNode__valueSlider = document.querySelector('.value-slider');
 let timeout__sliderHandler;
 
 
 function handler__sliderChanging() {
     clearTimeout(timeout__sliderHandler);
     timeout__sliderHandler = setTimeout(() => {
-        let elementsArray__slides = document.querySelectorAll('.w-slide');
-        // let el__activeSlide = document.querySelector('.');
+        let elementsArray__slides = document.querySelectorAll('.value-slider__slide.w-slide');
 
         elementsArray__slides.forEach((el__slide, id__slide) => {
             if (el__slide.getAttribute('aria-hidden') == null) {
@@ -22,10 +21,10 @@ function handler__sliderChanging() {
 
 
 
-let observerOptions = {
+let observerOptions__valueSlider = {
     childList: true,
     attributes: true,
     subtree: true
 }
-let observer = new MutationObserver(handler__sliderChanging);
-observer.observe(targetNode, observerOptions);
+let observer__valueSlider = new MutationObserver(handler__sliderChanging);
+observer__valueSlider.observe(targetNode__valueSlider, observerOptions__valueSlider);
